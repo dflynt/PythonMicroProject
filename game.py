fd6b7c3facb59fb2424136aa9215a09a149e6f11
Daniel Flynt <danielflynt1@gmail.com>
2018-03-08 13:02:21 -0500

    map is created

---
+++ game.py
@@ -0,0 +1,46 @@
+import sys
+
+class Game:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.size = width * height
+        self.board = []
+
+    def populateBoard(self):
+        for i in range(0, self.width):
+            self.board.append([])
+            for l in range(0, self.height):
+                if l == 0 or l == self.width - 1: #top and bottom row boundaries
+                    self.board[i].append(" 0 ")
+                elif i == 0 or i == self.height - 1: #first and last column boundaries
+                    self.board[i].append(" 0 ")
+                else:
+                    self.board[i].append(" 1 ")
+
+        # hard code entry and exit
+        # line 23 = exit
+        # line 25 = entry           
+        self.board[0][self.width - 2] = " 1 "
+        self.board[self.height - 1][1] = " 1 "
+
+    # hard code boundaries
+    def createBoundaries(self):
+        for x in range(2,6):
+            self.board[2][x] = " 0 "
+        for x in range(3, 7):
+            self.board[5][x] = " 0 "
+        
+    def printBoard(self):
+        for line in range(0, self.width):
+              print(' '.join(self.board[line])) #.join on space removes brackets, commas, and quotes when printing lists
+
+#Board width and height is taken from command line arguments
+#[1] is width
+#[2] is height
+print(sys.argv[1])
+print(sys.argv[2])
+game = Game(int(sys.argv[1]), int(sys.argv[2]))
+game.populateBoard()
+game.createBoundaries()
+game.printBoard()
